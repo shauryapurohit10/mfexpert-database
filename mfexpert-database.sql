f8a5f187815209144c1f555514dcbcdd09585707
Shaurya Purohit <shaurya.purohit@sls.ads.valuelabs.net>
2019-03-07 11:08:24 +0530

    creating database repo

---
+++ mfexpert-database.sql
@@ -0,0 +1,12 @@
+-- Database: mfexpert
+
+-- DROP DATABASE mfexpert;
+
+CREATE DATABASE mfexpert
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
